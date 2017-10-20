bde4aa67ec01004563957f26f859ac34ac90262b
NatasjaVanherck <Natasja.Vanherck@student.pxl.be>
2017-10-20 12:24:18 +0200

    Update 1.0 - Challenge 3 af

--- src/be/pxl/streams/Challenge3.java
+++ src/be/pxl/streams/Challenge3.java
@@ -2,6 +2,8 @@ package be.pxl.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.Stream;
 
 public class Challenge3 {
 	public static void main(String[] args) {
@@ -29,6 +31,20 @@ public class Challenge3 {
 		// 3. Hoeveel namen beginnen er met A
 		// Verwachte output: 2
 		
+		topNames.stream().sorted()
+						   .map(s -> s = s.substring(0, 1).toUpperCase() + s.substring(1,s.length()))
+						   .forEach(System.out::println);
 		
+		System.out.println("*********");
+		
+		topNames.stream().map(s -> s = s.substring(0, 1).toUpperCase() + s.substring(1,s.length()))
+		                 .filter(s -> s.contains("A"))
+		                 .forEach(System.out::println);
+		
+		System.out.println("*********");
+		
+		long hoeveel = topNames.stream().filter(s -> s.substring(0,1).contains("A"))
+        				                .count();
+		System.out.println(hoeveel);
 	}
 }
