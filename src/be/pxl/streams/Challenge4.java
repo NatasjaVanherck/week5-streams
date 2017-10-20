335d2fc3b28007c236bb0e3c24f5bd6efa5f47d5
NatasjaVanherck <Natasja.Vanherck@student.pxl.be>
2017-10-20 13:20:15 +0200

    update 1.0 - Challenge 4 af

--- src/be/pxl/streams/Challenge4.java
+++ src/be/pxl/streams/Challenge4.java
@@ -2,6 +2,7 @@ package be.pxl.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 
 import be.pxl.streams.Person.Gender;
 
@@ -9,31 +10,54 @@ public class Challenge4 {
 	public static void main(String[] args) {
 		List<Person> personen = Arrays.asList(
 				new Person("Sofie", 23, Gender.FEMALE),
-				new Person("Adam", 31, Gender.MALE), 
+				new Person("Adam", 31, Gender.MALE),
 				new Person("Bastiaan", 25, Gender.MALE),
-				new Person("Berend", 22, Gender.MALE), 
-				new Person("Aagje", 27, Gender.FEMALE)
-		);
-		// 1. Geef de gemiddelde leeftijd van alle personen
-		// Verwachte output:  Gemiddelde leeftijd: 25.6
+				new Person("Berend", 22, Gender.MALE),
+				new Person("Aagje", 27, Gender.FEMALE));
 		
+		// 1. Geef de gemiddelde leeftijd van alle personen
+		// Verwachte output: Gemiddelde leeftijd: 25.6
+
+		OptionalDouble gemiddelde = personen.stream().mapToInt(s -> s.getAge())
+				                                     .average();
+		if (gemiddelde.isPresent()) {
+			System.out.println("Gemiddelde leeftijd: " + gemiddelde.getAsDouble());
+		} else {
+			System.out.println("No result");
+		}
 		
 		// 2. Hoeveel mannen staan er in de lijst
 		// Verwachte output: Aantal mannen: 3
-		
+
+		long hoeveelMannen = personen.stream().map(s -> s.getGender()).filter(t -> (t == Gender.MALE)).count();
+		System.out.println("Aantal mannen: " + hoeveelMannen);
 		
 		// 3. Hoeveel mannen ouder dan 24 staan er in de lijst
-		// Verwachte output: Aantal mannen boven 24: 2
-		
+		// Verwachte output: Aantal mannen boven 24: 3
+
+		long hoeveelMannenOuder = personen.stream().filter(t -> (t.getAge() > 24))
+													.map(s -> s.getGender())
+				                                   .count();
+		System.out.println("Aantal mannen boven 24: " + hoeveelMannenOuder);
 		
 		// 4. Geef de gemiddelde leeftijd van alle mannen
 		// Gemiddelde leeftijd mannen: 26.0
+
+
+		OptionalDouble gemiddeldeMan = personen.stream().filter(t -> (t.getGender() == Gender.MALE))
+				                              .mapToInt(s -> s.getAge())
+                                              .average();
+		System.out.println("Gemiddelde leeftijd mannen: " + gemiddeldeMan.getAsDouble());
 		
-		
-		// 5. Maak een nieuwe persoon met als naam de eerste letter van iedere persoon in de lijst
+		// 5. Maak een nieuwe persoon met als naam de eerste letter van iedere
+		// persoon in de lijst
 		// en als leeftijd de som van alle leeftijden
 		// Maak gebruik van de methode .reduce()
-		
 
+		String name = personen.stream().map(s -> s.getName())
+				                        .reduce("", (acc, el) -> acc + el.substring(0,1));
+		int leeftijd = personen.stream().mapToInt(s -> s.getAge())
+										.sum();
+		Person newPerson = new Person(name, leeftijd, Gender.FEMALE);
 	}
 }
