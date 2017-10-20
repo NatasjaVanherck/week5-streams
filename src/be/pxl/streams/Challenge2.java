3169faf11aa2e1185467f7cbf9da2da66b0657b9
NatasjaVanherck <Natasja.Vanherck@student.pxl.be>
2017-10-20 12:09:17 +0200

    Update 1.0 Challenge2 af

--- src/be/pxl/streams/Challenge2.java
+++ src/be/pxl/streams/Challenge2.java
@@ -1,5 +1,9 @@
 package be.pxl.streams;
 
+import java.util.OptionalInt;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Challenge2 {
 	public static void main(String[] args) {
 		// maak een stream met 10 random gehele getallen tussen 0 en 30
@@ -8,7 +12,20 @@ public class Challenge2 {
 		// en geef het maximum 
 		// gebruik eventueel peek() om een tussenresultaat van de stream te tonen
 		
+		Random rand = new Random();
+		int[] randomGetallen = new int[10];
 		
+		for(int i = 0; i < randomGetallen.length; i++){
+			randomGetallen[i] = rand.nextInt(29) +1;
+		}
 		
+		OptionalInt max = IntStream.of(randomGetallen).filter(s -> (s % 3 == 0))
+								                      .max();
+		
+		if(max.isPresent()){
+			System.out.println(max.getAsInt());
+		} else {
+			System.out.println("No result");
+		}
 	}
 }
